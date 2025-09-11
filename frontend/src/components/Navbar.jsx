322f76345cc1e776fb16d91b31d09599e74c740c
Maheedhar <maheedharambadipudi756@gmail.com>
2025-09-11 14:25:40 +0530

    14th commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
-function Navbar() {
+import { useAuthStore } from "../store/useAuthStore";
+
+const Navbar = () => {
+  const { logout, authUser } = useAuthStore();
+
   return (
-    <div>
-      Navbar
-    </div>
+    <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80">
+
+    </header>
   )
 }
 
