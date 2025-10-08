dc5f7a1d13e9e1efeb0c4d5e5e1122970ef6bb8e
Maheedhar <maheedharambadipudi756@gmail.com>
2025-10-08 15:33:46 +0530

    30th

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -2,6 +2,7 @@ import { useChatStore } from "../store/useChatStore"
 import { useEffect } from "react"
 import ChatHeader from "./ChatHeader"
 import MessagesInput from "./MessagesInput"
+import MessageSkeleton from "./skeletons/MessageSkeleton"
 
 const ChatContainer = () => {
   const { messages, getMessages, isMessagesLoading, selectedUser } = useChatStore()
@@ -10,7 +11,13 @@ const ChatContainer = () => {
     getMessages(selectedUser._id)
   }, [selectedUser._id, getMessages])
 
-  if(isMessagesLoading) return <div>loading...</div>
+  if(isMessagesLoading) return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+      <MessageSkeleton />
+      <MessagesInput />
+    </div>
+  )
 
   return (
     <div className="flex-1 flex flex-col overflow-auto">
