a630d1f36bc2194a69dc3729951cba85449d047c
Maheedhar <maheedharambadipudi756@gmail.com>
2025-10-28 10:07:04 +0530

    39th com

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser())
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"http://localhost:5173" || "https://chatify-sable.vercel.app/",
   credentials: true
 }));
 
