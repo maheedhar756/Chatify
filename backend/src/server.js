39e1c7c267bc1a1d8f3a2ee0b823b7265ae58649
Maheedhar <maheedharambadipudi756@gmail.com>
2025-10-31 12:12:45 +0530

    Final changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,10 +19,18 @@ app.use(cors({
 }));
 
 const PORT = process.env.PORT || 5001
-const __dirname = 
+const __dirname = path.resolve();
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  })
+}
 
 server.listen(PORT, () => {
   console.log("server is running on port " + PORT)
